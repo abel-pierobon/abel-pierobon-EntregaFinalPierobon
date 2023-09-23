e7128ea9215c369ba31fde437994405ce23234f0
abel-pierobon <130199147+abel-pierobon@users.noreply.github.com>
2023-09-23 17:54:32 -0300

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,7 +5,7 @@ import Item from './Item';
 
 function ItemList(props) {
     return (
-            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2">
+            <section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2">
                 {props.data.length === 0 ? (
                     <div className='flex justify-center'>
                         <p className="font-bold texto-aparecer-desaparecer"> CARGANDO...</p>
